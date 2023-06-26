a492c6efbd89f839ccd843892b5b7b56360487ef
El-gibbor <chiagoziem11@yahoo.com>
2023-06-26 08:57:28 +0400

    added a struct template for the format func members

--- main.h
+++ main.h
@@ -7,6 +7,18 @@
 int putChar(char c);
 int printStrLiteral(char *str);
 int _printf(const char *format, ...);
+int (*getFormatFunc(char c))(va_list);
 int printChar(char c);
+
+/**
+ * struct format - struct template for format
+ * @c: character to return its corresponding fomat func
+ * @formatFunc: func pointer to the corresponding format func
+*/
+typedef struct format
+{
+    char c;
+    int (*formatFunc)(va_list);
+} formatStruct;
 
 #endif
