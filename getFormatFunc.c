c7b9c1b47173a6fd3f5ae969058d2d414d659d7d
El-gibbor <chiagoziem11@yahoo.com>
2023-06-26 08:55:58 +0400

    progress with func ptr. struct array of func ptrs

---
+++ getFormatFunc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * getFormatFunc - returns a pointer the corresponding format func
+ * @c: character to return its corresponding fomat func
+ * Return: func pointer to the corresponding format func
+ */
+int (*getFormatFunc(char c))(va_list)
+{
+    int i = 0;
+    formatStruct format[] = {
+        {'c', printChar},
+        {'s', printStrLiteral},
+        {'\0', NULL} /* sentinal value for termination*/
+    };
+
+    for (; format[i].c; i++)
+    {
+        if (format[i].c == c)
+            return (format[i].formatFunc);
+    }
+    return (NULL);
+}
