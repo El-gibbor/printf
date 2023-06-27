7a431057f76d4a449fc9dd25708a58310ec80786
teflon40 <frimpong.bernard3220@gmail.com>
2023-06-27 13:43:10 +0000

    Added a branch

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef PRINT_F
+#define PRINT_F
+
+/* Header files.. */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/* function types */
+int print_ch(char);
+int print_int(va_list);
+int print_str(va_list);
+int _printf(const char *format, ...);
+
+#endif
