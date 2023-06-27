f5843b123372c6c032641b62e384702e8dd62827
El-gibbor <chiagoziem11@yahoo.com>
2023-06-27 22:43:18 +0400

    test case for NULL string

--- testFile/main.c
+++ testFile/main.c
@@ -2,7 +2,13 @@
 
 int main(void)
 {
-	int len = 0, len2 = 0, len3 = 0, len4 = 0;
+	int len = 0, len10 = 0, len20 = 0, len2 = 0, len3 = 0, len4 = 0;
+
+	len10 = _printf("it's NULL here %s\n", NULL);
+	len20 = printf("it's NULL here %s\n", NULL);
+
+	_printf("%d\n", len10);
+	printf("%d\n", len20);
 
 	len = _printf("A %c%c%s sentence\n", 's', 'i', "mple");
 	len2 = printf("A %c%c%s sentence\n", 's', 'i', "mple");
